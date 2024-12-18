63d12d1aaf63eead60abd72190548f7e05dbd8b1
Mitin Aleksandr <jamanphonehtc@gmail.com>
2024-12-19 00:48:30 +0300

    KS-1-4 Задача 4 к Уроку 1
    * Убрал лишние комментарии.
    * Убрал U-типы.

--- src/main/kotlin/lesson1/Task4.kt
+++ src/main/kotlin/lesson1/Task4.kt
@@ -2,23 +2,12 @@ package lesson1
 
 fun main() {
 
-  /**@value Расстояние, которое пролетел Гагарин, пока был в космосе в миллиметрах*/
-  val length: ULong = 40_868_600_000u
-
-  /**@value Возраст Гагарина на момент полета (целых лет)*/
-  val age: UByte = 27u
-
-  /**@value Какую часть дня Гагарин провел в космосе*/
+  val length: Long = 40_868_600_000
+  val age: Byte = 27
   val partOfDay: Float = 0.075f
-
-  /**@value количество секунд которое Гагарин провел в космосе*/
-  val seconds:UShort = 6480u
-
-  /**@value какую часть года Гагарин провел в космосе*/
+  val seconds:Short = 6480
   val partOfYear:Double = 2.0547945205479453E-4
-
-  /**@value апогей орбиты Гагарина в метрах.*/
-  val apogee:UInt  = 327000u
+  val apogee:Int  = 327000
 
   println(length)
   println(age)
